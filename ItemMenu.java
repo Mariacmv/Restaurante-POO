0ec5e9ae75ca07978482f2dc1fa6f661f148bd7c
Gabriel Cézar <151453514+gbrielzera@users.noreply.github.com>
2024-09-12 11:19:34 -0300

    ItemMenu.java

--- ItemMenu.java
+++ ItemMenu.java
@@ -1,25 +1,41 @@
 package br.com.restaurante;
+
 public class ItemMenu {
     private String nome;
     private String categoria;
     private double preco;
 
-    public ItemMenu(String nome, String categoria, double preco){
+    // Construtor com parâmetros
+    public ItemMenu(String nome, String categoria, double preco) {
         this.nome = nome;
         this.categoria = categoria;
         this.preco = preco;
-    }
-    public String setDetalhesItem(String nome, String categoria, double preco){
-        this.nome = nome;
-        this.categoria = categoria;
-        this.preco = preco;
-        return nome;
-    }
-    public String getDetalhesItem(){
-        return "Nome: "+nome+ ", Categoria: " +categoria+ ", Preçço: " +preco;
     }
 
-    public double calcularPreco(){
+    // Método getDetalhesItem (para retornar detalhes do item)
+    public String getDetalhesItem() {
+        return nome + " (" + categoria + ") - R$" + preco;
+    }
+
+    // Método getter para retornar o preço
+    public double getPreco() {
         return preco;
+    }
+
+    // Getters e Setters para nome e categoria, se necessário
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
     }
 }
