ae1e00973fa74ca0d6f68ad4673fb727f00938b6
Gabriel Cézar <151453514+gbrielzera@users.noreply.github.com>
2024-09-12 11:20:06 -0300

    Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,57 +1,57 @@
-package br.com.restaurante;
-
-public class Cliente {
-    // Atributos privados
-    private String nome;
-    private String endereco;
-    private String telefone;
-
-    // Construtor com parâmetros
-    public Cliente (String nome, String endereco, String telefone){
-        this.nome = nome;
-        this.endereco = endereco;
-        this.telefone = telefone;
+    package br.com.restaurante;
+    
+    public class Cliente {
+        // Atributos privados
+        private String nome;
+        private String endereco;
+        private String telefone;
+    
+        // Construtor com parâmetros
+        public Cliente (String nome, String endereco, String telefone){
+            this.nome = nome;
+            this.endereco = endereco;
+            this.telefone = telefone;
+        }
+    
+        // Método registrarCliente()
+        public void registrarCliente(String nome, String endereco, String telefone) {
+            this.nome = nome;
+            this.endereco = endereco;
+            this.telefone = telefone;
+        }
+    
+        // Métodos getter e setter
+        // Nome
+        public String getNome(){
+            return nome;
+        }
+    
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+    
+        // Endereço
+        public String getEndereco(){
+            return endereco;
+        }
+    
+        public void setEndereco(String endereco) {
+            this.endereco = endereco;
+        }
+    
+        // Telefone
+        public String getTelefone(){
+            return telefone;
+        }
+    
+        public void setTelefone(String telefone) {
+            this.telefone = telefone;
+        }
+    
+        // Método visualizarCliente()
+        public String visualizarCliente(){
+            return "Nome: " + nome + "\nEndereço: " + endereco + "\nTelefone: " + telefone;
+        }
+    
+    
     }
-
-    // Método registrarCliente()
-    public void registrarCliente(String nome, String endereco, String telefone) {
-        this.nome = nome;
-        this.endereco = endereco;
-        this.telefone = telefone;
-    }
-
-    // Métodos getter e setter
-    // Nome
-    public String getNome(){
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    // Endereço
-    public String getEndereco(){
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    // Telefone
-    public String getTelefone(){
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    // Método visualizarCliente()
-    public String visualizarCliente(){
-        return "Nome: " + nome + "\nEndereço: " + endereco + "\nTelefone: " + telefone;
-    }
-
-
-}
